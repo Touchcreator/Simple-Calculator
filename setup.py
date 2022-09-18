e25d3af5fe8046bf19d41670cf0956918c81cfe5
TouchIsNotAGoodCoder <64277067+Touchcreator@users.noreply.github.com>
2022-09-18 14:46:37 -0400

    change some dumb things

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
  
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
+  'Development Status :: 7 - Inactive',
   'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'Operating System :: OS Independent',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
  
 setup(
   name='touchs-simple-calculator',
-  version='22.9.17',
+  version='22.9.18',
   description='A simple calculator library made with the tutorial from Joshua Lowe.',
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='https://github.com/Touchcreator/Simple-Calculator',  
